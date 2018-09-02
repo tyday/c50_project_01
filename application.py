3aebbac5fc0d645f1b88c92cfc09e25c68802be1
Ty Day <tyrday@gmail.com>
2018-09-01 23:20:21 -0400

    added login ability

--- application.py
+++ application.py
@@ -1,12 +1,17 @@
 import os
+import config
 
-from flask import Flask, session
+# import functools
+from flask import Flask, session, redirect, escape, url_for, request, render_template, flash
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-app = Flask(__name__)
+from werkzeug.security import check_password_hash, generate_password_hash
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_URL
+app.debug = True
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
@@ -17,10 +22,85 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
+# engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 db = scoped_session(sessionmaker(bind=engine))
 
 
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    # return "Project 1: TODO"
+    if 'username' in session:
+        user=session['username']
+    else:
+        user=None
+    return render_template('index.html', user=user)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    # remove the username fro the session if it is there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+@app.route('/login/auth', methods = ['GET', 'POST'])
+def login_auth():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+
+        if not username:
+            error = 'Username is required'
+        elif not password:
+            error = 'Password is required'
+        selection = db.execute(
+            'SELECT * FROM users WHERE username=:param', ({"param":username})).fetchone() 
+        password_check = check_password_hash(selection.password, password)
+        if password_check is False:
+            error = 'password is incorrect'
+        
+        if error is None and password_check is True:
+            session['username'] = username
+            return redirect(url_for('index'))
+        print('line before flash', error)
+        flash(error)
+
+    # flights = db.execute('SELECT * FROM flights WHERE id=4')
+    # retValue = ""
+    # for flight in flights:
+    #     retValue += flight.origin
+    return render_template('login.html', error = error)
+@app.route('/register')
+def register():
+    return render_template('register.html')
+@app.route('/register/auth', methods = ['GET','POST'])
+def register_auth():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+
+        if not username:
+            error = 'Username is required'
+        elif not password:
+            error = 'Password is required'
+        elif db.execute(
+            'SELECT * FROM users WHERE username=:param', ({"param":username})
+        ).fetchone() is not None:
+            error = 'User {} is already registered'.format(username)
+        
+        if error is None:
+            db.execute(
+                'INSERT INTO users (username, password, role) VALUES (:param1, :param2, :param3)',
+                {"param1":username, "param2":generate_password_hash(password), "param3":'writer'}
+            )
+            # db.execute(users.insert(),{"username":username,"password":password})
+            db.commit()
+            return redirect(url_for('login'))
+        flash(error)
+    return render_template('register.html', error = error)
+if __name__== '__main__':
+    app.run()
