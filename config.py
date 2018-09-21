9f941953d5e9759e5d0270f5bf37e72d14540e3f
Ty Day <tyrday@gmail.com>
2018-09-21 16:26:29 -0400

    removed config.py

--- config.py
+++
@@ -1,2 +0,0 @@
-# DATABASE_URL="postgres://kzftxhqlaehnoe:528d362b84d194fcf9fd723eb1407405d96d843ba59c5ba5b0f6dd3989b27698@ec2-54-225-76-201.compute-1.amazonaws.com:5432/ddi5vh8kpp2sqi"
-DATABASE_URL="postgres://postgres:mBP9!u46@localhost"
